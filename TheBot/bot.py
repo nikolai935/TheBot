8cbd2fccc00b0ca647a08606b6cf831cacc35c84
Nikolai Dale Milde <ndalemilde@outlook.com>
2020-02-08 01:37:34 +0100

    Importing discord.ext
    Running the bot

--- TheBot/bot.py
+++ TheBot/bot.py
@@ -1,1 +1,14 @@
 import discord
+from discord.ext import commands
+from discord.ext.commands import has_permissions, MissingPermissions
+token = open("key.txt", "r").read()
+
+client = commands.Bot(command_prefix='!')
+
+
+@client.event
+async def on_ready():
+    print("{0.user} is online!".format(client))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game("awesome!"))
+
+client.run(token)
